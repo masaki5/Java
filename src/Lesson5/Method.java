6610e52f9567036e553fb93441d50883eefb6e09
masaki5 <xxtmhxx5@gmail.com>
2021-02-24 21:40:40 +0900

    kadai4

---
+++ src/Lesson5/Method.java
@@ -0,0 +1,16 @@
+package Lesson5;
+
+public class Method {
+    public static void methodA(){
+        System.out.println("methodA");
+        methodB();
+    }
+
+    public static void methodB(){
+        System.out.println("methodB");
+    }
+
+    public static void main(String[]args){
+        methodA();
+    }
+}
