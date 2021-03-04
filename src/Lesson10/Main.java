330bba4217bbabdc23f2d2ca2578fc325a830b23
masaki5 <xxtmhxx5@gmail.com>
2021-03-04 22:57:24 +0900

    kadai103

--- src/Lesson10/Main.java
+++ src/Lesson10/Main.java
@@ -2,6 +2,8 @@ package Lesson10;
 
 public class Main {
     public static void main(final String[]args){
+        Hero h = new Hero();
+        h.run();
         SuperHero sh = new SuperHero();
         sh.run();
     }
--- src/Lesson10/SuperHero.java
+++ src/Lesson10/SuperHero.java
@@ -14,4 +14,26 @@ public class SuperHero extends Hero{
         System.out.println("着地した!");
     }
 
+    public void run(){
+        System.out.println(this.name + "は撤退した!");
+    }
+
+//    public void attack(Matango m){
+//        System.out.println(this.name + "の攻撃");
+//        m.hp -= 5;
+//        System.out.println("5ポイントのダメージを与えた");
+//        if (this.flying){
+//            System.out.println(this.name + "の攻撃");
+//            m.hp -= 5;
+//            System.out.println("5ポイントのダメージを与えた");
+//        }
+//    }
+//    上記省略
+    public void attack(Matango m){
+        super.attack(m);
+        if (this.flying){
+            super.attack(m);
+        }
+    }
+
 }
