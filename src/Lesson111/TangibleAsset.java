8a96f281e2ca7ce9437a5c3025936238ec76c5ca
masaki5 <xxtmhxx5@gmail.com>
2021-03-18 22:47:32 +0900

    kadai116

---
+++ src/Lesson111/TangibleAsset.java
@@ -0,0 +1,15 @@
+package Lesson111;
+
+public class TangibleAsset {
+    String name;
+    int price;
+    String color;
+    public TangibleAsset(String name, int prce, String color) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+    }
+    public String getName(){return this.name;}
+    public int getPrice(){return this.price;}
+    public String getColor(){return this.color;}
+}
