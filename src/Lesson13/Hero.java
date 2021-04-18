9052faba42a41636d266ed91876132658f006cec
masaki5 <xxtmhxx5@gmail.com>
2021-04-18 22:57:56 +0900

    kadai134

--- src/Lesson13/Hero.java
+++ src/Lesson13/Hero.java
@@ -8,9 +8,15 @@ public class Hero {
     public String getName(){
         return this.name;
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
     public void bye() {
         System.out.println("勇者は別れを告げた");
     }
+
     private void die() {
         System.out.println(this.name + "は死んでしまった！");
         System.out.println("GAME OVERです。");
